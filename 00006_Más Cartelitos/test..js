efcf3d67e5332858cfb63f63374912d82b914240
mayra@mumuki.org <mayra@mumuki.org>
2022-04-29 14:15:16 +0000

    Mumuki Export on 2022-04-29 14:15:16 +0000

--- 00006_Más Cartelitos/test..js
+++ 00006_Más Cartelitos/test..js
@@ -1,13 +1,12 @@
-  
-  def test_escribir_cartelito_Dra_Ana_Perez_false_es_Dra_Ana_Perez(self):
-    self.assertEqual(escribir_cartelito("Dra.", "Ana", "Perez", False), "Dra. Ana Perez")
-  
-  def test_escribir_cartelito_Dr_Julio_Gelman_false_es_Dr_Julio_Gelman(self):
-    self.assertEqual(escribir_cartelito("Dr.", "Julio", "Gelman", False), "Dr. Julio Gelman")
-  
-  def test_escribir_cartelito_Dra_Ana_Perez_true_es_Dra_Perez_(self):
-    self.assertEqual(escribir_cartelito("Dra.", "Ana", "Perez", True), "Dra. Perez")
-  
-  def test_escribir_cartelito_Dr_Julio_Gelman_true_es_Dr_Gelman_(self):
-    self.assertEqual(escribir_cartelito("Dr.", "Julio", "Gelman", True), "Dr. Gelman")
-  
+it("escribirCartelito(\"Dra.\", \"Ana\", \"Pérez\", false), es \"Dra. Ana Pérez\"", () => {
+  assert.equal(escribirCartelito("Dra.", "Ana", "Pérez", false), "Dra. Ana Pérez")
+})
+it("escribirCartelito(\"Dr.\", \"Julio\", \"Gelman\", false), es \"Dr. Julio Gelman\"", () => {
+  assert.equal(escribirCartelito("Dr.", "Julio", "Gelman", false), "Dr. Julio Gelman")
+})
+it("escribirCartelito(\"Dra.\", \"Ana\", \"Pérez\", true), es \"Dra. Pérez\"", () => {
+  assert.equal(escribirCartelito("Dra.", "Ana", "Pérez", true), "Dra. Pérez")
+})
+it("escribirCartelito(\"Dr.\", \"Julio\", \"Gelman\", true), es \"Dr. Gelman\"", () => {
+  assert.equal(escribirCartelito("Dr.", "Julio", "Gelman", true), "Dr. Gelman")
+})
