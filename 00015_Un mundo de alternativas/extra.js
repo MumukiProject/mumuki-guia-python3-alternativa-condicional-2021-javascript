6a94fe13febb30d9cec46eb13b2055aee2be5fca
mayra@mumuki.org <mayra@mumuki.org>
2022-04-29 14:46:26 +0000

    Mumuki Export on 2022-04-29 14:46:26 +0000

--- 00015_Un mundo de alternativas/extra.js
+++ 00015_Un mundo de alternativas/extra.js
@@ -1,19 +1,22 @@
 def listo(): 
   pass
 
-def saludar_a(quien, horario):
-  if horario < 12:
-    return "Buenos días " + quien
-  elif horario < 19:
-    return "Buenas tardes " + quien
-  else: 
-    return "Buenas noches " + quien
-    
-def saludar_a_recargado(quien, horario):
-  if horario < 19:
-    return "Buenas tardes " + quien
-  elif horario < 12:
-    return "Buenos días " + quien
-  else: 
-    return "Buenas noches " + quien
+function saludarA(quien, horario) {
+  if (horario < 12){
+    return "Buenos días " + quien;
+  } else if (horario < 19) {
+    return "Buenas tardes " + quien;
+  } else {
+    return "Buenas noches " + quien;
+  }
+}
+
+function saludarARecargado(quien, horario) {
+  if (horario < 19){
+    return "Buenas tardes " + quien;
+  } else if (horario < 12) {
+    return "Buenos días " + quien;
+  } else {
+    return "Buenas noches " + quien;
+  }
     
