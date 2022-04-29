9b787143246449e66b08abfaf766585c659442c9
mayra@mumuki.org <mayra@mumuki.org>
2022-04-29 13:08:43 +0000

    Mumuki Export on 2022-04-29 13:08:43 +0000

--- 00014_Hora de tomar una decisión/extra.js
+++ 00014_Hora de tomar una decisión/extra.js
@@ -1,5 +1,7 @@
-def saludar_a(quien, horario):
-  if horario < 19:
-    return "Buenos días " + quien
-  else:
-    return "Buenas noches " + quien
+function saludarA(quien, horario){
+  if (horario < 19){
+    return "Buenos días " + quien;
+  }else{
+    return "Buenas noches " + quien;
+  }
+}
